fdc547b4d353d1bfd6da5dcd81a2129e26019d74
divyanshraheja69 <divyanshraheja15@gmail.com>
2024-02-07 15:46:39 +0530

    homepagework

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import ExploreQuickStayCoLiving from "./pages/home/explore/ExploreQuickStayCoLiving";
-import Header from "./components/navbar/Header";
-import BelowHeader from "./components/navbar/BelowHeader";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <BelowHeader />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
